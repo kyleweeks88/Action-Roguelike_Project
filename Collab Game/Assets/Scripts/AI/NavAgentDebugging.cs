07235b44c0a2a43f7957ba6899c08fed9a9fce4d
Kyle Weeks <60368775+kyleweeks88@users.noreply.github.com>
2021-04-08 09:05:27 -0700

    4.8_AI prototyping

---
+++ Collab Game/Assets/Scripts/AI/NavAgentDebugging.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentDebugging : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent navAgent;
+
+    public bool velocity;
+    public bool desiredVelocity;
+    public bool path;
+
+    private void OnDrawGizmosSelected()
+    {
+        if(velocity)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + navAgent.velocity);
+        }
+
+        if(desiredVelocity)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + navAgent.desiredVelocity);
+        }
+
+        if(path)
+        {
+            Gizmos.color = Color.black;
+            var agentPath = navAgent.path;
+            Vector3 prevCorner = transform.position;
+            foreach (var corner in agentPath.corners)
+            {
+                Gizmos.DrawLine(prevCorner, corner);
+                Gizmos.DrawSphere(corner, 0.1f);
+                prevCorner = corner;
+            }
+        }
+    }
+}
--- Collab Game/Assets/Scripts/AI/NpcController.cs
+++ Collab Game/Assets/Scripts/AI/NpcController.cs
@@ -7,18 +7,108 @@ public class NpcController : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] NavMeshAgent navAgent;
+    [SerializeField] Animator animator;
+    [SerializeField] Transform raycastPos;
 
+    public float walkSpeed;
+    public float runSpeed;
+    public float sprintSpeed;
     public float sightRange;
 
     public bool targetInSight;
+    public bool debug;
+
+    float distToPlayer;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        navAgent.speed = walkSpeed;
     }
 
     private void Update()
     {
-        navAgent.destination = player.transform.position;
+        distToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        CanSeeTarget();
+
+        DetermineSpeed();
+
+        animator.SetFloat("moveSpeed", navAgent.velocity.magnitude);
+    }
+
+    bool CanSeeTarget()
+    {
+        bool boolVal = false;
+        targetInSight = boolVal;
+
+        if (distToPlayer <= sightRange)
+        {
+            Ray sightRay = new Ray(raycastPos.position, -(raycastPos.position - player.GetComponent<CapsuleCollider>().bounds.center));
+            RaycastHit hit;
+            if(Physics.Raycast(sightRay, out hit, sightRange))
+            {
+                if(hit.transform.gameObject.tag == "Player")
+                {
+                    navAgent.destination = player.transform.position;
+                    targetInSight = true;
+                    return targetInSight;
+                }
+                else
+                {
+                    navAgent.destination = transform.position;
+                    targetInSight = false;
+                    return targetInSight;
+                }
+            }
+        }
+        else
+        {
+            navAgent.destination = transform.position;
+            targetInSight = false;
+            return targetInSight;
+        }
+
+        return targetInSight;
+    }
+
+    void DetermineSpeed()
+    {
+        if (targetInSight)
+        {
+            if (distToPlayer >= sprintSpeed)
+            {
+                navAgent.speed = sprintSpeed;
+            }
+            else if (distToPlayer >= runSpeed)
+            {
+                navAgent.speed = runSpeed;
+            }
+            else
+            {
+                navAgent.speed = walkSpeed;
+            }
+        }
+        else
+        {
+            navAgent.speed = walkSpeed;
+        }
+    }
+
+    IEnumerator SearchForTarget()
+    {
+        yield return new WaitForSeconds(.25f);
+
+        
+        yield return null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(debug)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, sightRange);
+        }
     }
 }
