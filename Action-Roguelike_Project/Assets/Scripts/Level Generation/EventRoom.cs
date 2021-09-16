192374a2fc37f59cae7c14bcdc59550dad7f1bb3
Kyle Weeks <60368775+kyleweeks88@users.noreply.github.com>
2021-09-16 13:29:03 -0700

    9.14_general
    refining

--- Action-Roguelike_Project/Assets/Scripts/Level Generation/EventRoom.cs
+++ Action-Roguelike_Project/Assets/Scripts/Level Generation/EventRoom.cs
@@ -4,4 +4,15 @@ public class EventRoom : Room
 {
     // EVENT ROOM CODE
     public string roomName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            foreach (Doorway doorway in doorways)
+            {
+                doorway.gameObject.SetActive(true);
+            }
+        }
+    }
 }
