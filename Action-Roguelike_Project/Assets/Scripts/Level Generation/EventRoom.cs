66e7734515c78eeb3dbd9b51e95660e40a9d8b50
Kyle Weeks <60368775+kyleweeks88@users.noreply.github.com>
2021-09-20 09:59:24 -0700

    9.17_General

--- Action-Roguelike_Project/Assets/Scripts/Level Generation/EventRoom.cs
+++ Action-Roguelike_Project/Assets/Scripts/Level Generation/EventRoom.cs
@@ -1,9 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EventRoom : Room
 {
-    // EVENT ROOM CODE
     public string roomName;
+    [SerializeField] Transform eventSpawnLocation;
+    LevelGenerator levelGenerator;
+
+    //List<GameObject> eventPrefabPool = new List<GameObject>();
+
+    private void Start()
+    {
+        levelGenerator = GetComponentInParent<LevelGenerator>();
+        if(levelGenerator != null)
+        {
+            Debug.Log(levelGenerator.transform.name);
+            foreach (var item in levelGenerator.roomEventPrefabs)
+            {
+                Debug.Log(item.name);
+            }
+        }
+        else
+        {
+            Debug.LogError("Error");
+        }
+    }
+
+    void SpawnEventObject()
+    {
+        int rand = Random.Range(0, levelGenerator.roomEventPrefabs.Count);
+        GameObject eventObject = Instantiate(levelGenerator.roomEventPrefabs[rand], eventSpawnLocation.position, eventSpawnLocation.rotation);
+        eventObject.transform.parent = this.transform;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +41,9 @@ public class EventRoom : Room
             {
                 doorway.gameObject.SetActive(true);
             }
+
+            SpawnEventObject();
+            print("Entered");
         }
     }
 }
---
+++ Action-Roguelike_Project/Assets/Scripts/Level Generation/RoomEvent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomEvent : MonoBehaviour
+{
+
+    // RoomEvent will choose a random event to happen.
+
+    // These events will load data from Scriptable Objects?
+
+    
+}
